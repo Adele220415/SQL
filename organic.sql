bd490267c538d4168c1a81a09565b296f96ceef0
Adele220415 <oluwatumininusolanke@gmail.com>
2025-06-04 11:36:44 +0100

    Add files via upload

---
+++ organic.sql
@@ -0,0 +1,15 @@
+USE Organic
+SELECT * FROM Customers
+
+-- DROP TABLE Customers
+INSERT INTO Customers (Name, PhoneNumber)
+VALUES('Aisha Oven Dried', 8052362431),
+      ('2-1 Chicken', 8037510320),
+      ('Abdul Phase4', 9155884346),
+      ('Aderemi Victor', 9056303815),
+      ('Al Hassan 5 weeks', 9064944729),
+      ('Ayaola 2-1 police station', 8166184686);
+
+-- CREATE TABLE Customers (
+-- Name NVARCHAR(50) ,
+-- PhoneNumber DECIMAL(10, 2)  );
